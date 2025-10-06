d3d95658ad33a84ca31152876a3df1928ce8f7c4
rohanjackie <52731713+rohanjackie@users.noreply.github.com>
2025-10-05 22:09:15 -0700

    Time: 705 ms (13.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with cte as (
+    select *, lead(num,1) over() as next_1, lead(num,2) over() as next_2
+    from logs
+)
+
+select distinct(num) as ConsecutiveNums
+from cte
+where num = next_1
+and num = next_2
